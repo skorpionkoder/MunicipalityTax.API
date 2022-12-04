2181f0688435759870f78bbd17c0983ad4622144
skorpionkoder <karshankhanil@gmail.com>
2022-12-04 18:22:15 +0530

    Add required fields

--- MunicipalityTax.API/Models/MunicipalityTaxModel.cs
+++ MunicipalityTax.API/Models/MunicipalityTaxModel.cs
@@ -1,12 +1,17 @@
-﻿using MunicipalityTax.API.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using MunicipalityTax.API.Data.Entities;
 
 namespace MunicipalityTax.API.Models
 {
     public class MunicipalityTaxModel
     {
+        [Required]
         public string MunicipalityName { get; set; }
+        [Required]
         public string TaxType { get; set; }
+        [Required]
         public decimal TaxAmount { get; set; }
+        [Required]
         public DateTime StartDtm { get; set; }
     }
 }
